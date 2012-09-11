30c91b0ae75cfe82dccc6aaad54500a7c23acd82
Gav Wood <gav@lancasterlogicresponse.com>
2012-09-11 19:37:21 +0100

    Support for Nero AAC encoder.

--- sync.sh
+++ sync.sh
@@ -5,7 +5,8 @@
 # The GNU GPL version 3 is defined at http://www.gnu.org/licenses/gpl-3.0.html
 
 mp3stream="lame preset=medium"
-aacstream="faac profile=2 ! ffmux_mp4"
+aacstream="faac bitrate=64000 ! ffmux_mp4"
+usenero=0
 stream=""
 extension=""
 
@@ -23,11 +24,12 @@ function USAGE ()
 {
     echo ""
     echo "USAGE: "
-    echo "    ${0/*\/} [-s|-x] <from> <to>"
+    echo "    ${0/*\/} [-n|-s|-x] <from> <to>"
     echo ""
     echo "OPTIONS:"
     echo "    -s <stream>    Specify custom gstreamer pipeline"
     echo "    -x <extension> Specify filename extension (default mp3)"
+    echo "    -n             Use the Nero HE-AAC encoder (requires FLAC inputs for now)"
     echo "    <from>         Specify source path for files"
     echo "    <to>           Specify destination path for files"
     echo "    -h             This usage information"
@@ -39,9 +41,10 @@ function USAGE ()
     exit $E_OPTERROR    # Exit and explain usage, if no argument(s) given.
 }
 
-while getopts ":s:x:h:v" Option
+while getopts "ns:x:hv" Option
 do
     case $Option in
+        n    ) usenero=1;;
         x    ) extension="$OPTARG";;
         s    ) stream="$OPTARG";;
         v    ) VERSION
@@ -62,7 +65,11 @@ if [[ "x$source" == "x" ]]; then USAGE; fi
 if [[ "x$dest" == "x" ]]; then USAGE; fi
 
 if [[ "x$extension" == "x" ]]; then
-	extension=mp3
+	if [[ $usenero -gt 0 ]]; then
+		extension=mp4
+	else
+		extension=mp3
+	fi
 fi
 
 if [[ "x$stream" == "x" ]]; then
@@ -97,12 +104,22 @@ do
 	d="$dest/${s%.*}.$extension"
 	mkdir -p "${d%/*}"
 	printf "\rEncoding: %6s/$total" $i
-	nice -n 19 gst-launch filesrc "location=$s" ! decodebin ! $stream ! id3v2mux ! filesink "location=$dest/.incoming" >/tmp/.sync-out-$$ 2>/dev/null
+	incoming="$dest/.incoming"
+	if [[ $usenero -gt 0 ]] ; then
+		incoming="$dest/.incoming.mp4"
+		mknod "/tmp/p-$$" p
+		nice -n 19 gst-launch filesrc "location=$s" ! decodebin ! wavenc ! filesink "location=/tmp/p-$$" 1>/dev/null 2>/dev/null &
+		nice -n 19 neroAacEnc -q 0.15 -if "/tmp/p-$$" -of "$incoming" 1>/dev/null 2>/tmp/.sync-out-$$ && 
+		nice -n 19 metaflac --export-tags-to=- "$s" | while read tag; do echo -n - && printf "meta-user:$tag\0"; done | xargs -0 neroAacTag "$incoming" 1>/dev/null 2>/dev/null
+		rm -f "/tmp/p-$$"
+	else
+		nice -n 19 gst-launch filesrc "location=$s" ! decodebin ! $stream ! id3v2mux ! filesink "location=$incoming" >/tmp/.sync-out-$$ 2>/dev/null
+	fi
 	
-	[[ `grep Interrupt /tmp/.sync-out-$$` ]] && { echo "Interrupted."; exit; }
+	[[ `grep Interrupt /tmp/.sync-out-$$` ]] && { echo ""; echo "Interrupted."; rm -f /tmp/.sync-out-$$; exit; }
 	
 	rm -f /tmp/.sync-out-$$
-	mv "$dest/.incoming" "$d"
+	mv "$incoming" "$d"
 	
 	if [[ ! -e "${d%/*}/cover.jpg" && -e "${s%/*}/cover.jpg" ]]
 	then
